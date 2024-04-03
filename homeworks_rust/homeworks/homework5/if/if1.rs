18ec8de2a0c7866b45dc6e220c548e3e3fce0412
Christopher-KT <164919408+Christopher-KT@users.noreply.github.com>
2024-04-02 21:52:08 -0600

    Update if1.rs
    added if statement to fn bigger

--- homeworks_rust/homeworks/homework5/if/if1.rs
+++ homeworks_rust/homeworks/homework5/if/if1.rs
@@ -3,6 +3,10 @@
 // I AM NOT DONE
 
 pub fn bigger(a: i32, b: i32) -> i32 {
+    if a > b {
+        return a
+    }
+    b
     // Complete this function to return the bigger number!
     // Do not use:
     // - another function call
